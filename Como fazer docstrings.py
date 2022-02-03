26bc19598e6506250a119ad8373ee1a1803d9e54
brunobendel <75317932+brunobendel@users.noreply.github.com>
2022-02-03 14:26:32 -0300

    aula 21 curso em video

--- Como fazer docstrings.py
+++ Como fazer docstrings.py
@@ -1,5 +1,12 @@
 
 def contador(i,f,p):
+    """
+    Contador feito Pelo Bruno na aula do guanabara:
+    :param i: parametro inicial
+    :param f: parametro final
+    :param p: Passo
+    :return: printa os valores do inicio ao fim contando pelo passo.
+    """
     c = i
     while c<=f:
         print(f'{c}', end=" ")
@@ -7,4 +14,4 @@ def contador(i,f,p):
     print('Fim!')
 
 contador(0,10,2)
-
+help(contador)
